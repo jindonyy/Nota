7e141bcbabbebedafe7068dddaa946796e44adf9
jindonyy <rsefa94@gmail.com>
2024-11-10 00:45:25 +0900

    [chore] next.config path 에러 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     sassOptions: {
