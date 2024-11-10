e585c16858a9dd5bba367593c0e7797ceecceea4
jindonyy <rsefa94@gmail.com>
2024-11-10 23:32:09 +0900

    [chore] react-query 설치 및 설정

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,9 +1,14 @@
 import { MswProvider } from './MswProvider';
 
+import ReactQueryProvider from '@/providers/ReactQueryProvider';
 import type { PropsWithChildren } from 'react';
 
 export default function Providers(props: PropsWithChildren) {
     const { children } = props;
 
-    return <MswProvider>{children}</MswProvider>;
+    return (
+        <ReactQueryProvider>
+            <MswProvider>{children}</MswProvider>
+        </ReactQueryProvider>
+    );
 }
