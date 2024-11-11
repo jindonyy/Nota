4af43a40bd96966f712d1a029ef648c6999fc559
jindonyy <rsefa94@gmail.com>
2024-11-11 14:34:43 +0900

    [feat] fetch module 추가

---
+++ src/modules/serverFetch.ts
@@ -0,0 +1,28 @@
+import { HttpError } from '@/modules/HttpError';
+
+export const serverFetch = async <T>(...args: Parameters<typeof fetch>) => {
+    const url = (process.env.NEXT_PUBLIC_MOCK_API_SERVER_URL ?? '') + args[0];
+    const options = {
+        ...args[1],
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+            Pragma: 'no-cache',
+            ...args[1]?.headers,
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new HttpError(response.status, response.statusText);
+        }
+        return response.json() as T;
+    } catch (error) {
+        if (error instanceof HttpError) {
+            console.error(`Fetch error: ${error.message} (Status: ${error.status})`);
+        }
+        throw error;
+    }
+};
