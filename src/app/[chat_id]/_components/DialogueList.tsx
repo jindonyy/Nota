176a953e570392459cb7a488a8621e9eb07531e4
jindonyy <rsefa94@gmail.com>
2024-11-12 18:56:06 +0900

    [feat] chat 페이지 진입 시 최하단으로 이동

--- src/app/[chat_id]/_components/DialogueList.tsx
+++ src/app/[chat_id]/_components/DialogueList.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { useParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 import { useGetChat } from '@/app/[chat_id]/_apis';
 import DialogueItem from '@/app/[chat_id]/_components/DialogueItem';
+
 import './DialogueList.scss';
 
 export default function DialogueList() {
@@ -11,6 +13,12 @@ export default function DialogueList() {
     const {
         data: { data },
     } = useGetChat(params.chat_id);
+
+    useEffect(() => {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+        });
+    }, []);
 
     return (
         <div className="dialogue-list">
