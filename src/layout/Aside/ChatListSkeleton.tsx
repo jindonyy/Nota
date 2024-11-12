cab1f45bbc57bf4c633e89ff45562cd4958976d4
jindonyy <rsefa94@gmail.com>
2024-11-13 02:00:37 +0900

    [design] chat list skeleton UI 수정

--- src/layout/Aside/ChatListSkeleton.tsx
+++ src/layout/Aside/ChatListSkeleton.tsx
@@ -3,11 +3,13 @@ import { Skeleton } from '@/components/shadcn/skeleton';
 export default function ChatListSkeleton() {
     return (
         <div className="aside-chat-list-skeleton">
-            {Array.from({ length: 10 }).map((_, index) => (
+            {Array.from({ length: 4 }).map((_, index) => (
                 // eslint-disable-next-line react/no-array-index-key
                 <div key={index} className="aside-chat-item-skeleton">
                     <Skeleton className="aside-chat-item-title-skeleton" />
-                    <Skeleton className="aside-chat-item-model-skeleton" />
+                    <div className="aside-chat-item-model-skeleton">
+                        <Skeleton />
+                    </div>
                 </div>
             ))}
         </div>
