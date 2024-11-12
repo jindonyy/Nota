93d3f5adfddfff69ccda02137a648010407d4f9a
jindonyy <rsefa94@gmail.com>
2024-11-12 18:55:18 +0900

    [feat] client api param next 옵션에서 react query 옵션으로 수정

--- src/app/[chat_id]/_apis/chat.ts
+++ src/app/[chat_id]/_apis/chat.ts
@@ -1,18 +1,23 @@
 'use client';
 
+import type { UseMutationOptions, UseSuspenseQueryOptions } from '@tanstack/react-query';
 import { useMutation, useSuspenseQuery } from '@tanstack/react-query';
 
 import type { Chat } from '@/app/[chat_id]/_models';
 import { queryKeys } from '@/constants/queryKeys';
 import { clientFetch } from '@/modules';
+import type { HttpError } from '@/modules/HttpError';
 import type { Data } from '@/types/data';
 
 export type GetChatsResponse = Data<Chat[]>;
 
-export const useGetChats = (init?: RequestInit) => {
+export const useGetChats = (
+    options?: Omit<UseSuspenseQueryOptions<GetChatsResponse, Error, GetChatResponse, string[]>, 'queryKey'>,
+) => {
     const result = useSuspenseQuery({
         queryKey: [queryKeys.getChats],
-        queryFn: async () => await clientFetch<GetChatsResponse>('/chats', init),
+        queryFn: async () => await clientFetch<GetChatsResponse>('/chats'),
+        ...options,
     });
 
     return result;
@@ -20,10 +25,14 @@ export const useGetChats = (init?: RequestInit) => {
 
 export type GetChatResponse = Data<Chat>;
 
-export const useGetChat = (chatId: string, init?: RequestInit) => {
+export const useGetChat = (
+    chatId: string,
+    options?: Omit<UseSuspenseQueryOptions<GetChatResponse, Error, GetChatResponse, string[]>, 'queryKey'>,
+) => {
     const result = useSuspenseQuery({
         queryKey: [queryKeys.getChat, chatId],
-        queryFn: async () => await clientFetch<GetChatResponse>(`/chats/${chatId}`, init),
+        queryFn: async () => await clientFetch<GetChatResponse>(`/chats/${chatId}`),
+        ...options,
     });
 
     return result;
@@ -31,14 +40,18 @@ export const useGetChat = (chatId: string, init?: RequestInit) => {
 export type PostDialoguesRequest = { prompt: string };
 export type PostDialoguesResponse = Data<Chat>;
 
-export const usePostChat = (chatId: string) => {
+export const usePostChat = (
+    chatId: string,
+    options?: UseMutationOptions<PostDialoguesResponse, HttpError, PostDialoguesRequest, unknown>,
+) => {
     const result = useMutation({
         mutationFn: (request: PostDialoguesRequest) =>
-            clientFetch(`/chats/${chatId}/dialogues`, {
+            clientFetch<PostDialoguesResponse>(`/chats/${chatId}/dialogues`, {
                 method: 'POST',
                 body: JSON.stringify(request),
             }),
         gcTime: 0,
+        ...options,
     });
 
     return result;
