c3adbfc77223bf02188afb110069d98d72b1a91c
jindonyy <rsefa94@gmail.com>
2024-11-13 00:09:47 +0900

    [feat] home 페이지 UI 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import './page.scss';
+
 export default function Home() {
-    return <div>{'hi'}</div>;
+    return (
+        <div className="home-page">
+            <img
+                className="home-logo"
+                src="https://static.wixstatic.com/media/6ee621_46b49dcb2b1e4bb8aa12c802e71492b0~mv2.png/v1/fill/w_210,h_210,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Nota%20AI_YouTube%20profile_4_edited.png"
+                alt="Nota AI logo"
+            />
+        </div>
+    );
 }
