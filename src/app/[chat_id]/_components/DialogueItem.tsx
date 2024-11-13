a516518ea1e4c0a56c37f669eb9981ba9a1cfffd
jindonyy <rsefa94@gmail.com>
2024-11-13 22:13:50 +0900

    [feat] Nota 로고 next static 이미지로 변경

--- src/app/[chat_id]/_components/DialogueItem.tsx
+++ src/app/[chat_id]/_components/DialogueItem.tsx
@@ -14,10 +14,7 @@ export default function DialogueItem(props: Props) {
                 <Message text={data.prompt} hasProfile={false} />
             </div>
             <div className="chat-dialogue-completion">
-                <Message
-                    text={data.completion}
-                    profileImageUrl="https://static.wixstatic.com/media/6ee621_46b49dcb2b1e4bb8aa12c802e71492b0~mv2.png/v1/fill/w_210,h_210,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Nota%20AI_YouTube%20profile_4_edited.png"
-                />
+                <Message text={data.completion} profileImageUrl="/static/images/nota_profile.webp" />
             </div>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import Image from 'next/image';
 import './page.scss';
 
 export default function Home() {
     return (
         <div className="home-page">
-            <img
+            <Image
                 className="home-logo"
-                src="https://static.wixstatic.com/media/6ee621_46b49dcb2b1e4bb8aa12c802e71492b0~mv2.png/v1/fill/w_210,h_210,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Nota%20AI_YouTube%20profile_4_edited.png"
+                width="100"
+                height="100"
+                src="/static/images/nota_profile.webp"
                 alt="Nota AI logo"
             />
         </div>
