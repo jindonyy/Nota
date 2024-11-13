a8bd2d1ea535da7e4c1e1d7488e9c0577f1612ea
jindonyy <rsefa94@gmail.com>
2024-11-14 02:02:28 +0900

    [feat] 전역 에러 페이지 추가

---
+++ src/app/error.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import './error.scss';
+import { Button } from '@/components/shadcn/button';
+
+export default function ErrorPage({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+    const handleReload = () => {
+        reset();
+    };
+
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+
+    return (
+        <div className="shop">
+            <div className="error-page">
+                <p className="error-page-text">
+                    로드 중 오류
+                    <br />
+                    다시 시도해주세요.
+                </p>
+                <Button onClick={handleReload} size="lg">
+                    <div>{'새로고침'}</div>
+                </Button>
+            </div>
+        </div>
+    );
+}
